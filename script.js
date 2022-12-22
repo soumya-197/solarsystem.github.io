bb183f994b0515fe194c230a022fc6a5afb3fb5f
soumya-197 <101653368+soumya-197@users.noreply.github.com>
2022-12-22 22:56:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function openMer(){
+    closecard()
+    document.getElementById("mer").style.borderColor="white";
+    document.getElementById("card1").style.display="block";
+    
+}
+function openV(){
+    closecard()
+    document.getElementById("v").style.borderColor="white";
+    document.getElementById("card2").style.display="block";
+}
+function openE(){
+    closecard()
+    document.getElementById("e").style.borderColor="white";
+    document.getElementById("card3").style.display="block";
+}
+function openMars(){
+    closecard()
+    document.getElementById("mar").style.borderColor="white";
+    document.getElementById("card4").style.display="block";
+}
+function openJup(){
+    closecard()
+    document.getElementById("j").style.borderColor="white";
+    document.getElementById("card5").style.display="block";
+}
+function openSat(){
+    closecard()
+    document.getElementById("s").style.borderColor="white";
+    document.getElementById("card6").style.display="block";
+}
+function openU(){
+    closecard()
+    document.getElementById("u").style.borderColor="white";
+    document.getElementById("card7").style.display="block";
+}
+function openNep(){
+    closecard()
+    document.getElementById("n").style.borderColor="white";
+    document.getElementById("card8").style.display="block";
+}
+function closecard(){
+    document.getElementById("mer").style.borderColor="#333030";
+    document.getElementById("v").style.borderColor="#333030";
+    document.getElementById("e").style.borderColor="#333030";
+    document.getElementById("mar").style.borderColor="#333030";
+    document.getElementById("j").style.borderColor="#333030";
+    document.getElementById("s").style.borderColor="#333030";
+    document.getElementById("u").style.borderColor="#333030";
+    document.getElementById("n").style.borderColor="#333030";
+    document.getElementById("card1").style.display="none";
+    document.getElementById("card2").style.display="none";
+    document.getElementById("card3").style.display="none";
+    document.getElementById("card4").style.display="none";
+    document.getElementById("card5").style.display="none";
+    document.getElementById("card6").style.display="none";
+    document.getElementById("card7").style.display="none";
+    document.getElementById("card8").style.display="none";
+}
